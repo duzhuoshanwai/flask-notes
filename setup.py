8c06cf26f8b23062690d63efee306259f4e21306
duzhuoshanwai <duzhuo@saint1337.top>
2025-06-12 12:51:50 +0800

    fix: 为Markdown编辑器添加ID标识并更新表单内容字段

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flask-note",
+    version="2025.06.12",
+    packages=find_packages(),
+    install_requires=[
+        "Flask==3.1.1",
+        "python-dotenv==1.1.0",
+        "Authlib==1.6.0",
+        "Flask-WTF==1.2.2",
+        "PyJWT==2.10.1",
+        "requests==2.32.4",
+        "MarkupSafe==3.0.2",
+        "mistune==3.1.3",
+        "gunicorn==23.0.0",
+        "bleach==6.2.0",
+    ],
+)
